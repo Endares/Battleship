6be0ebb4056b3a5bd02cfb6aacade491314d93e0
endares <syzheng722@gmail.com>
2025-02-07 21:33:47 -0500

    task8

--- app/src/test/java/org/battleship/BoardTextViewTest.java
+++ app/src/test/java/org/battleship/BoardTextViewTest.java
@@ -42,6 +42,22 @@ class BoardTextViewTest {
     }
 
     @Test
+    public void test_display_nonempty_3by3() {
+        Board<Character> b1 = new BattleShipBoard<Character>(3, 3);
+        BoardTextView view = new BoardTextView(b1);
+
+        b1.tryAddShip(new BasicShip(new Coordinate(1, 1)));
+        String expectedHeader = "  0|1|2\n";
+        String expectedBody =
+                        "A  | |  A\n" +
+                        "B  |s|  B\n" +
+                        "C  | |  C\n";
+
+        String expected = expectedHeader + expectedBody + expectedHeader;
+        assertEquals(expected, view.displayMyOwnBoard());
+    }
+
+    @Test
     public void test_invalid_board_size() {
         Board<Character> wideBoard = new BattleShipBoard<Character>(11,20);
         Board<Character> tallBoard = new BattleShipBoard<Character>(10,27);
